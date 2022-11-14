c6db9c816531cd2f5f7a2c34056c4ca50ec8726d
onthegrid007 <peter.ferranti.dev@gmail.com>
2022-11-14 16:52:43 -0500

    .

--- FlagParser.h
+++ FlagParser.h
@@ -2,7 +2,6 @@
 *   BSD 3-Clause License, see file labled 'LICENSE' for the full License.
 *   Copyright (c) 2022, Peter Ferranti
 *   All rights reserved.
-*   Other Contributers:
 */
 
 #include <string>
